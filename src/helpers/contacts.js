f0eb6ddc4f3733ff8b8a6f324a7f4e6070240836
Marlon <marlonchiodelli@hotmail.com>
2019-12-20 10:19:04 -0200

    Commit Contacts

--- src/helpers/contacts.js
+++ src/helpers/contacts.js
@@ -9,24 +9,4 @@ export default [
         "name": "Marlon Chiodelli (Vivo)",
         "phone": "(54) 99991-9525"
     },
-    {
-        "id": 3,
-        "name": "teste 1",
-        "phone": "(54) 99140-3970"
-    },
-    {
-        "id": 4,
-        "name": "teste 2",
-        "phone": "(54) 99991-9525"
-    },
-    {
-        "id": 5,
-        "name": "teste 3",
-        "phone": "(54) 99140-3970"
-    },
-    {
-        "id": 6,
-        "name": "teste 4",
-        "phone": "(54) 99991-9525"
-    },
 ]
